de08ba335a5735ac65c6a871b73d3a6f671ae7c2
Paul-Ndirangu <paulmwaura254@gmail.com>
2024-01-08 10:24:46 +0300

    langcorn commit

---
+++ langchain.py
@@ -0,0 +1,18 @@
+from langchain.chains import LLMChain
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+
+
+llm = OpenAI(temperature=0.9)
+prompt = PromptTemplate(
+    input_variables=["product"],
+    template = "What is a good name for a company that makes {product}?",
+)
+
+
+
+chain = LLMChain(llm=llm, prompt=prompt)
+
+if __name__ == "__main__":
+    print(chain.run("ice cream"))
+    

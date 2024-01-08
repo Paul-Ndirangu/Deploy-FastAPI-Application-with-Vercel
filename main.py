d8d970804774b2961c88b0b372936a26148a3d3f
Paul-Ndirangu <paulmwaura254@gmail.com>
2024-01-08 10:27:13 +0300

    .

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-import uvicorn 
-from fastapi import FastAPI
-
-app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World!"}
-
-@app.post("/home")
-async def home():
-    return {"message": "Hello from FastAPI!"}
+# import uvicorn 
+# from fastapi import FastAPI
+from langcorn import create_service
 
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+app = create_service(
+    "llm_chain:chain",
+    "conversation_chian:conversation"
+)
+
+
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World!"}
+
+# @app.post("/home")
+# async def home():
+#     return {"message": "Hello from FastAPI!"}
+
+
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
     

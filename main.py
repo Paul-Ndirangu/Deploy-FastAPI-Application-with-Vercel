1df0eb6aa8cf509c202c3caad3ef3c7f7b370a96
Paul-Ndirangu <paulmwaura254@gmail.com>
2024-01-08 10:46:20 +0300

    .

--- main.py
+++ main.py
@@ -2,19 +2,7 @@ from langcorn import create_service
 
 app = create_service(
     "llm_chain:chain",
-    "conversation_chian:conversation"
+    "conversation_chain:conversation"
 )
 
-
-# @app.get("/")
-# async def root():
-#     return {"message": "Hello World!"}
-
-# @app.post("/home")
-# async def home():
-#     return {"message": "Hello from FastAPI!"}
-
-
-# if __name__ == "__main__":
-#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
-    
+ 
